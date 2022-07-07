f9ebff7e616a4942a0e7ef4822a461b8d95ac440
Artem Bulgakov <artyombulgakov@gmail.com>
2022-07-07 13:39:29 +0300

    add favicon; loading animation; new project

---
+++ js/on_loading.js
@@ -0,0 +1,20 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value, all) {
+    var element = document.getElementById(id);
+    element.style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true, false);
+    show('loading', false, true);
+});
